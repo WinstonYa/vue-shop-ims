223a9d2ce47cee2a072bf68dfc5e048f87dd392d
WinstonYa <2215444192@qq.com>
2020-04-17 16:28:39 +0800

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="login-container">
+    <div class="login-box">
+      <!-- 头像区域 -->
+      <div class="avatar-box">
+        <img src="../assets/logo.jpg" alt="" />
+      </div>
+      <!-- 登录表单区域 -->
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginFormRules"
+        label-width="0px"
+        class="login-form"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="el-icon-user"
+          ></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginForm.password"
+            prefix-icon="el-icon-lock"
+            type="password"
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      //登录表单数据绑定对象
+      loginForm: {
+        username: "zs",
+        password: "123"
+      },
+      //表单验证规则对象
+      loginFormRules: {
+        //验证用户名
+        username: [
+          {
+            required: true,
+            message: "请输入登录名称",
+            trigger: "blur"
+          },
+          {
+            min: 3,
+            max: 10,
+            message: "长度在 3 到 10 个字符",
+            trigger: "blur"
+          }
+        ],
+        //验证密码
+        password: [
+          {
+            required: true,
+            message: "请输入登录密码",
+            trigger: "blur"
+          },
+          {
+            min: 6,
+            max: 15,
+            message: "长度在 6 到 15 个字符",
+            trigger: "blur"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    //点击重置登录表单
+    resetLoginForm() {
+      // console.log(this)
+      this.$refs.loginFormRef.resetFields();
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.login-container {
+  background-color: #2b4b6b;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.login-box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+
+  .avatar-box {
+    width: 130px;
+    height: 130px;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+    }
+  }
+}
+
+.login-form {
+  margin-top: 110px;
+  padding: 0 30px;
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
