92741d8f45c1388e08eb59ba243cb53638345c2a
WinstonYa <2215444192@qq.com>
2020-04-29 20:22:30 +0800

    completedList

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -100,6 +100,7 @@
             </el-form-item>
           </el-tab-pane>
           <el-tab-pane label="商品图片" name="3">
+            <!-- action 表示图片要上传到的后台API地址 -->
             <el-upload
               :action="uploadURL"
               :on-preview="handlePreview"
@@ -112,21 +113,27 @@
             </el-upload>
           </el-tab-pane>
           <el-tab-pane label="商品内容" name="4">
-            <quill-editor v-model="addForm.goods_introduce"> </quill-editor>
+            <!-- 富文本编辑器组件 -->
+            <quill-editor v-model="addForm.goods_introduce"></quill-editor>
             <!-- 添加商品的按钮 -->
-            <el-button type="primary" class="btnAdd">添加商品</el-button>
+            <el-button type="primary" class="btnAdd" @click="add"
+              >添加商品</el-button
+            >
           </el-tab-pane>
         </el-tabs>
       </el-form>
     </el-card>
 
+    <!-- 图片预览 -->
     <el-dialog title="图片预览" :visible.sync="previewVisible" width="50%">
-      <img :src="previewPath" alt="" class="prewviewImg" />
+      <img :src="previewPath" alt="" class="previewImg" />
     </el-dialog>
   </div>
 </template>
 
 <script>
+import _ from 'lodash'
+
 export default {
   data() {
     return {
@@ -139,10 +146,11 @@ export default {
         goods_number: 0,
         // 商品所属的分类数组
         goods_cat: [],
-        //图片的数组
+        // 图片的数组
         pics: [],
-        //商品的详细描述
-        goods_introduce: ''
+        // 商品的详情描述
+        goods_introduce: '',
+        attrs: []
       },
       addFormRules: {
         goods_name: [
@@ -172,13 +180,13 @@ export default {
       manyTableData: [],
       // 静态属性列表数据
       onlyTableData: [],
-      //上传图片的url地址
+      // 上传图片的URL地址
       uploadURL: 'http://127.0.0.1:8888/api/private/v1/upload',
-      //图片上传组件的headers请求头对象
+      // 图片上传组件的headers请求头对象
       headerObj: {
         Authorization: window.sessionStorage.getItem('token')
       },
-      previewPath: {},
+      previewPath: '',
       previewVisible: false
     }
   },
@@ -250,31 +258,69 @@ export default {
         this.onlyTableData = res.data
       }
     },
-    //处理图片预览效果
+    // 处理图片预览效果
     handlePreview(file) {
       console.log(file)
       this.previewPath = file.response.data.url
       this.previewVisible = true
     },
-    //处理移除图片的操作
+    // 处理移除图片的操作
     handleRemove(file) {
-      console.log(file)
-      //1.获取将要删除的图片的临时路径
+      // console.log(file)
+      // 1. 获取将要删除的图片的临时路径
       const filePath = file.response.data.tmp_path
-      //2.从pics数组中，找到这个图片对应的索引值
+      // 2. 从 pics 数组中，找到这个图片对应的索引值
       const i = this.addForm.pics.findIndex(x => x.pic === filePath)
-      //3.调用数组的splice方法，把图片信息对象，从pics数组中移除
+      // 3. 调用数组的 splice 方法，把图片信息对象，从 pics 数组中移除
       this.addForm.pics.splice(i, 1)
       console.log(this.addForm)
     },
-    //监听图片上传成功的事件
+    // 监听图片上传成功的事件
     handleSuccess(response) {
       console.log(response)
-      //拼接得到一个图片信息对象
+      // 1. 拼接得到一个图片信息对象
       const picInfo = { pic: response.data.tmp_path }
-      //将图片信息对象，push到pics数组中
+      // 2. 将图片信息对象，push 到pics数组中
       this.addForm.pics.push(picInfo)
       console.log(this.addForm)
+    },
+    // 添加商品
+    add() {
+      this.$refs.addFormRef.validate(async valid => {
+        if (!valid) {
+          return this.$message.error('请填写必要的表单项！')
+        }
+        // 执行添加的业务逻辑
+        // lodash   cloneDeep(obj)
+        const form = _.cloneDeep(this.addForm)
+        form.goods_cat = form.goods_cat.join(',')
+        // 处理动态参数
+        this.manyTableData.forEach(item => {
+          const newInfo = {
+            attr_id: item.attr_id,
+            attr_value: item.attr_vals.join(' ')
+          }
+          this.addForm.attrs.push(newInfo)
+        })
+        // 处理静态属性
+        this.onlyTableData.forEach(item => {
+          const newInfo = { attr_id: item.attr_id, attr_value: item.attr_vals }
+          this.addForm.attrs.push(newInfo)
+        })
+        form.attrs = this.addForm.attrs
+        console.log(form)
+
+        // 发起请求添加商品
+        // 商品的名称，必须是唯一的
+        const { data: res } = await this.$http.post('goods', form)
+
+        if (res.meta.status !== 201) {
+          return this.$message.error('添加商品失败！')
+        }
+
+        this.$message.success('添加商品成功！')
+        this.$router.push('/goods')
+      })
     }
   },
   computed: {
@@ -293,7 +339,7 @@ export default {
   margin: 0 10px 0 0 !important;
 }
 
-.prewviewImg {
+.previewImg {
   width: 100%;
 }
 
